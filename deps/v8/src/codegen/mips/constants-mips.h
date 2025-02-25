13dc01a2ddf53a256b728ee1fee65be48a84e038
cn1095 <1095053105@qq.com>
2025-02-25 16:25:56 +0800

    Update constants-mips.h

--- deps/v8/src/codegen/mips/constants-mips.h
+++ deps/v8/src/codegen/mips/constants-mips.h
@@ -140,7 +140,7 @@ const uint32_t kLeastSignificantByteInInt32Offset = 3;
 namespace v8 {
 namespace internal {
 
-constexpr size_t kMaxPCRelativeCodeRangeInMB = 4096;
+constexpr size_t kMaxPCRelativeCodeRangeInMB = 1024;
 
 // -----------------------------------------------------------------------------
 // Registers and FPURegisters.
